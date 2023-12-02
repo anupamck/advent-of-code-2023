14efc78e00bdfc4d8435d051fb390fe17c11e6ff
anupamck <anupam.ck@gmailcom>
2023-12-02 13:40:32 +0100

    Rewrite solution in functional style

--- advent_of_code_2023/solutions/1.py
+++ advent_of_code_2023/solutions/1.py
@@ -2,30 +2,15 @@ import os
 
 
 def get_first_number(characters):
-    for i in range(len(characters)):
-        # If i is a numerical value, return it
-        if characters[i].isdigit():
-            return characters[i]
-    return None
+    return next((char for char in characters if char.isdigit()), None)
 
 
 def get_last_number(characters):
-    characters = characters[::-1]
-    for i in range(len(characters)):
-        # If i is a numerical value, return it
-        if characters[i].isdigit():
-            return characters[i]
-    return None
+    return next((char for char in reversed(characters) if char.isdigit()), None)
 
 
 def get_calibration_values(input_text):
-    calibration_values = []
-    for line in input_text:
-        first_number = get_first_number(line)
-        last_number = get_last_number(line)
-        whole_number = first_number + last_number
-        calibration_values.append(int(whole_number))
-    return calibration_values
+    return [int(get_first_number(line) + get_last_number(line)) for line in input_text]
 
 
 if __name__ == '__main__':
