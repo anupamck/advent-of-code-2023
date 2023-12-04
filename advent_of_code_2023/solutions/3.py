98bb0458fa544d9e227c242558ec23c239a9d555
anupamck <anupam.ck@gmailcom>
2023-12-04 10:00:21 +0100

    Refactor solution

--- advent_of_code_2023/solutions/3.py
+++ advent_of_code_2023/solutions/3.py
@@ -2,40 +2,44 @@ import re
 import os
 
 
-def get_number_of_cubes(colour, game):
+def get_max_cubes(line, colour):
     pattern = "\d+(?= " + colour + ")"
-    matches = re.search(pattern, game)
-    if matches:
-        return int(matches.group(0))
-    return 0
+    matches = re.findall(pattern, line)
+    matches_int = [int(item) for item in matches]
+    return max(matches_int)
 
 
-def is_game_possible(game):
-    red_cubes = get_number_of_cubes('red', game)
-    green_cubes = get_number_of_cubes('green', game)
-    blue_cubes = get_number_of_cubes('blue', game)
-    if red_cubes > red_cube_limit or blue_cubes > blue_cube_limit or green_cubes > green_cube_limit:
+def get_game_number(line):
+    pattern = r"(?<=Game\s)\d+"
+    game_number = re.search(pattern, line).group(0)
+    return int(game_number)
+
+
+def is_game_possible(line):
+    red_cube_limit = 12
+    green_cube_limit = 13
+    blue_cube_limit = 14
+    max_blue_cubes = get_max_cubes(line, "blue")
+    max_green_cubes = get_max_cubes(line, "green")
+    max_red_cubes = get_max_cubes(line, "red")
+    if max_blue_cubes > blue_cube_limit:
+        return False
+    max_green_cubes = get_max_cubes(line, "green")
+    if max_green_cubes > green_cube_limit:
+        return False
+    max_red_cubes = get_max_cubes(line, "red")
+    if max_red_cubes > red_cube_limit:
         return False
     return True
 
 
 if __name__ == '__main__':
     input_file_path = os.path.join("advent_of_code_2023", 'inputs', '3.txt')
-    red_cube_limit = 12
-    green_cube_limit = 13
-    blue_cube_limit = 14
     possible_games = []
     with open(input_file_path) as input_file:
         input_text = input_file.readlines()
         for line in input_text:
-            game_number = int(line.split(":", 1)[0].replace("Game ", ""))
-            game_details = line.split(":", 1)[1]
-            games = game_details.split(";")
-            has_impossible_game = False
-            for game in games:
-                if not is_game_possible(game):
-                    has_impossible_game = True
-            if not has_impossible_game:
-                possible_games.append(game_number)
-    print(possible_games)
+            game_number = get_game_number(line)
+            if is_game_possible(line):
+                possible_games.append(int(game_number))
     print(sum(possible_games))
